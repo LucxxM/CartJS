c4b981b933f92fc5b2f1d4e3a0e4f81cb97d73e7
Luc <luchio83@hotmail.fr>
2022-06-16 15:03:44 +0200

    fix addenventlistener on BtnRemove

--- script.js
+++ script.js
@@ -17,12 +17,16 @@ function cartBoxCreation(sourceItem, nameProduct, priceProduct, itemId, dataQuan
                                 <h3>${nameProductText}</h3>
                                 <div>
                                     <span>Price : ${priceProductItem}€ </span>
-                                    <button class="removeItem" id="removeItem${id}">remove</button>
+                                    <button data-event="not" class="removeItem" id="removeItem${id}">remove</button>
                                     <strong id="test${id}">Quantity : ${quantity}</strong>
                                     <code class="priceBox" id="test2${id}">Price : ${itemsCost}€</code>
                             </div>
                             </div>
                         </div>`;	
+
+
+
+                        
     cartBox.appendChild(cardBox);
 
     console.log(priceProductItem);
@@ -30,50 +34,60 @@ function cartBoxCreation(sourceItem, nameProduct, priceProduct, itemId, dataQuan
     let priceBoxs = document.querySelectorAll(".priceBox");
 
     let totalPrice = 0;
-    for(let i = 0; i < priceBoxs.length; i++){
-        totalPrice += parseInt(priceBoxs[i].innerText.slice(8, 11));
-    }
-    console.log(totalPrice);
-    document.querySelector("#priceTotalBox").innerText = `Total price : ${totalPrice}€`;
+        for(let i = 0; i < priceBoxs.length; i++){
+            totalPrice += parseInt(priceBoxs[i].innerText.slice(8, 11));
+        }
+        console.log(totalPrice);
+        document.querySelector("#priceTotalBox").innerText = `Total price : ${totalPrice}€`;
 
-    
+
     
     const btnRemoves = document.querySelectorAll(".removeItem");
     console.log(btnRemoves);
     
-    btnRemoves.forEach((btnRemove) => {
-            btnRemove.addEventListener("click", function(){
-            let priceProductItem = parseInt(priceProduct.innerText.slice(8, 11));
-            let idRemove = this.id.slice(10);
-            console.log(idRemove);
-            let qty = parseInt(document.querySelector("#box" + idRemove).getAttribute("data-quantity"));
-            console.log(qty);
-            qty--;
-            let itemsCost = priceProductItem * qty;
-            document.querySelector("#box" + idRemove).setAttribute("data-quantity", qty);
-            
-            if (qty > 0){
-                document.querySelector("#test" + idRemove).innerText = "Quantity : " + (qty);
-                document.querySelector("#test2" + idRemove).innerText = "price : " + (itemsCost);
-            }else{
+        btnRemoves.forEach((btnRemove) => { 
+                console.log(btnRemove.getAttribute("data-event"));
                 
-                btnRemove.parentElement.parentElement.parentElement.remove();
-                if (qty <0){
-                    qty = 0;
-                    document.querySelector("#box" + idRemove).setAttribute("data-quantity", qty);
+                console.log(btnRemove.getAttribute("data-event"));
+                
+                if (btnRemove.getAttribute("data-event") === "done"){
+                    console.log("done");
                 }
+                else{
+                btnRemove.setAttribute("data-event", "done");
+                btnRemove.addEventListener("click", function(){
+                let priceProductItem = parseInt(priceProduct.innerText.slice(8, 11));
+                let idRemove = this.id.slice(10);
+                console.log(idRemove);
+                let qty = parseInt(document.querySelector("#box" + idRemove).getAttribute("data-quantity"));
+                console.log(qty);
+                qty--;
+                let itemsCost = priceProductItem * qty;
+                document.querySelector("#box" + idRemove).setAttribute("data-quantity", qty);
+                
+                if (qty > 0){
+                    document.querySelector("#test" + idRemove).innerText = "Quantity : " + (qty);
+                    document.querySelector("#test2" + idRemove).innerText = "price : " + (itemsCost);
+                }else{
+                    
+                    btnRemove.parentElement.parentElement.parentElement.remove();
+                    if (qty <0){
+                        qty = 0;
+                        document.querySelector("#box" + idRemove).setAttribute("data-quantity", qty);
+                    }
+                }
+        
+                let priceBoxs = document.querySelectorAll(".priceBox");
+        
+                let totalPrice = 0;
+                for(let i = 0; i < priceBoxs.length; i++){
+                    totalPrice += parseInt(priceBoxs[i].innerText.slice(8, 12));
+                }
+                console.log(totalPrice);
+                document.querySelector("#priceTotalBox").innerText = `Total price : ${totalPrice}€`;
+                });
             }
-    
-            let priceBoxs = document.querySelectorAll(".priceBox");
-    
-            let totalPrice = 0;
-            for(let i = 0; i < priceBoxs.length; i++){
-                totalPrice += parseInt(priceBoxs[i].innerText.slice(8, 11));
-            }
-            console.log(totalPrice);
-            document.querySelector("#priceTotalBox").innerText = `Total price : ${totalPrice}€`;
-            });
-    });
+        });
 
    
 }    
@@ -133,8 +147,9 @@ const btnAdds = document.querySelectorAll(".AddCartBtn");
         let priceBoxs = document.querySelectorAll(".priceBox");
 
         let totalPrice = 0;
+
         for(let i = 0; i < priceBoxs.length; i++){
-            totalPrice += parseInt(priceBoxs[i].innerText.slice(8, 11));
+            totalPrice += parseInt(priceBoxs[i].innerText.slice(8, 12));
         }
         console.log(totalPrice);
         document.querySelector("#priceTotalBox").innerText = `Total price : ${totalPrice}€`;
